35a5f66c3c53c1492580eb6c474b884bdded9f57
Stephen Johnson <Stephen Johnson@LAPTOP-9P84QHKQ>
2022-12-15 21:28:29 -0600

    PE6.13

--- StephenJohnson_2200_PE6.13/src/Exercise06_13.java
+++ StephenJohnson_2200_PE6.13/src/Exercise06_13.java
@@ -6,16 +6,16 @@
  */
 public class Exercise06_13 {
     public static void main(String[] args) {
-    	System.out.printf("%-10s%8s\n", "i", "m(i)");
+    	System.out.printf("%4s%20s\n", "i", "m(i)");
         for (int i = 1; i <= 20; i++) {
-        	System.out.printf("%-10d%10.4f\n", i, sumSeries(i));
+        	System.out.printf("%4d%20.4f\n",i, m(i));
         }
     }
 
-    public static double m(int n) {
+    public static double m(int i) {
         double sum = 0;
-        for (double i = 1; i <= n; i++) {
-            sum += i / (i + 1);
+        for (double k = 1; k <= i; k++) {
+            sum += k / (k + 1.0);
         }
         
         return sum;
